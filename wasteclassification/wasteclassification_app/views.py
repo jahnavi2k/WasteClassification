eb0e42f809ca419b3b0f4b30578e171ee46a2ae2
Simran Saigal <simransaigal247@gmail.com>
2022-08-27 20:55:34 +0530

    Returning img

--- wasteclassification/wasteclassification_app/views.py
+++ wasteclassification/wasteclassification_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
+from django.http import FileResponse
 
 class FileView(APIView):
   parser_classes = (MultiPartParser, FormParser)
@@ -11,6 +12,8 @@ class FileView(APIView):
     file_serializer = FileSerializer(data=request.data)
     if file_serializer.is_valid():
       file_serializer.save()
+      response = FileResponse(open('media/Screenshot_from_2022-02-24_19-39-07.png', 'rb'))
+      return response
       return Response(file_serializer.data, status=status.HTTP_201_CREATED)
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
