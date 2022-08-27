17e422b067596862ebacdf6c067e170e248d11cf
Sristi <sristi0108@gmail.com>
2022-08-27 21:01:52 +0530

    Added ML model

--- wasteclassification/wasteclassification_app/models.py
+++ wasteclassification/wasteclassification_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+import os
+
+def path_and_rename(instance, filename):
+    upload_to = 'media'
+    ext = filename.split('.')[-1]
+    filename = "specimen" + ext
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
 
 class File(models.Model):
-  file = models.FileField(blank=False, null=False)
+  photo = models.ImageField(upload_to=path_and_rename)
   remark = models.CharField(max_length=20)
   timestamp = models.DateTimeField(auto_now_add=True)
